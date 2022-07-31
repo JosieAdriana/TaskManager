99587b6090d333e200eddeac93d70a44f1fc7f99
Josie Flôres <drijosie@gmail.com>
2022-07-31 18:17:33 -0300

    Desenvolvido Register Component

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/User';
+import { HttpClient } from '@angular/common/http';
+import { UuidService } from 'src/app/services/uuid.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  public form: User = {
+    uuid: '',
+    email: '',
+    name: '',
+    password: '',
+  }
+  constructor(
+    private http: HttpClient,
+    private uuid: UuidService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
+  submit() {
+    this.http.post<any>('http://localhost:3000/users', {
+      "id": this.uuid.generate(),
+      "name": this.form.name,
+      "email": this.form.email,
+      "password": this.form.password
+    }).subscribe(data => {
+      alert("Usuário Cadastrado");
 
+      this.router.navigate(['login']);
+    })
+  }
 }
