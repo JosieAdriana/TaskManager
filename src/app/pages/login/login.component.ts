f08f9b546c0b3a0d52afa2d692c7ab844bd583e3
Josie Flôres <drijosie@gmail.com>
2022-08-01 13:27:59 -0300

    Desenvolvido Register Component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/interfaces/User';
+
+const BASE_URL = "http://localhost:3000";
 
 @Component({
-  selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public form = {
+    email: '',
+    password: '',
+  }
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  submit() {
+    this.http.post<any>(`${BASE_URL}/signin`, {
+      "email": this.form.email,
+      "password": this.form.password
+    })
+    .subscribe(({accessToken, user}: { accessToken: string, user: User }) => {
+      console.log(user);
+      localStorage.setItem('token', accessToken);
+      localStorage.setItem('user', JSON.stringify(user));
+      this.router.navigate(['tasks']);
+    })
+  }
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/User';
+import { HttpClient } from '@angular/common/http';
+import { UuidService } from 'src/app/services/uuid.service';
+import { Router } from '@angular/router';
+
+const BASE_URL = "http://localhost:3000";
 
 @Component({
-  selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  public form: User = {
+    uuid: '',
+    email: '',
+    name: '',
+    password: '',
+  }
+  constructor(
+    private http: HttpClient,
+    private uuid: UuidService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
+  submit() {
+    this.http.post<any>(`${BASE_URL}/register`, {
+      "id": this.uuid.generate(),
+      "name": this.form.name,
+      "email": this.form.email,
+      "password": this.form.password
+    }).subscribe((user: User) => {
+      alert("Usuário Cadastrado");
 
+      this.router.navigate(['login']);
+    })
+  }
 }
